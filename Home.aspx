e166fed428ccf951cacc0eb9650d9e41056ed242
aniOfri <46932810+aniOfri@users.noreply.github.com>
2021-07-29 14:42:22 +0200

    Updated title and background

--- Home.aspx
+++ Home.aspx
@@ -2,8 +2,11 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
     <link rel="stylesheet" href="HomeStyle.css">
 </asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+<asp:Content ID="title" ContentPlaceHolderID="TitlePlaceHolder" runat="server">
     <div class="text">
         <h1 class="title">מציאות מדומה</h1>
     </div>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
 </asp:Content>
--- masterpage.Master.designer.cs
+++ masterpage.Master.designer.cs
@@ -33,6 +33,15 @@ namespace VR_Web_Project
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// TitlePlaceHolder control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder TitlePlaceHolder;
+
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
