d9bda7f8113d4928a2f82d87abb649225e441e88
aniOfri <ofri0608@gmail.com>
2021-09-05 12:00:50 +0300

    Fixed "next page" issue

--- Appointments.aspx.cs
+++ Appointments.aspx.cs
@@ -9,32 +9,31 @@ namespace VR_Web_Project
 {
     public partial class appointments : System.Web.UI.Page
     {
-        protected void Page_Init(object sender, EventArgs e)
-        {
-            Session["chooseDay"] = false;
-            Session["choosePartic"] = false;
-        }
-
         protected void Page_Load(object sender, EventArgs e)
         {
-            DateTime today = DateTime.Today;
-            date1.Text = today.AddDays(1).ToShortDateString();
-            date2.Text = today.AddDays(2).ToShortDateString();
-            date3.Text = today.AddDays(3).ToShortDateString();
-            date4.Text = today.AddDays(4).ToShortDateString();
-            date5.Text = today.AddDays(5).ToShortDateString();
-            date6.Text = today.AddDays(6).ToShortDateString();
-            date7.Text = today.AddDays(7).ToShortDateString();
+            if(!Page.IsPostBack){
+                Session["chooseDay"] = false;
+                Session["choosePartic"] = false;
+                DateTime today = DateTime.Today;
+                date1.Text = today.AddDays(1).ToShortDateString();
+                date2.Text = today.AddDays(2).ToShortDateString();
+                date3.Text = today.AddDays(3).ToShortDateString();
+                date4.Text = today.AddDays(4).ToShortDateString();
+                date5.Text = today.AddDays(5).ToShortDateString();
+                date6.Text = today.AddDays(6).ToShortDateString();
+                date7.Text = today.AddDays(7).ToShortDateString();
+
+            }
         }
 
         protected void ParticipantsOrder(object sender, EventArgs e)
         {
             Button btn = sender as Button;
             label1.Text = "מספר משתתפים נבחר: " + btn.Attributes["CustomParameter"].ToString();
-            Session["choosePartc"] = true;
+            Session["choosePartic"] = true;
 
-            if ((bool)Session["choosePartic"] && (bool)Session["chooseDay"]) {                 
-                label3.Style.Remove("opacity");
+            if ((bool)Session["choosePartic"] && (bool)Session["chooseDay"]) {
+                label3.CssClass = "span3";
             }
 
         }
@@ -47,7 +46,7 @@ namespace VR_Web_Project
 
             if ((bool)Session["choosePartic"] && (bool)Session["chooseDay"])
             {
-                label3.Style.Remove("opacity");
+                label3.CssClass = "span3";
             }
         }
     }
