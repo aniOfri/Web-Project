725415b3a3037a18e6a05472ba01793d0c92c549
aniOfri <ofri0608@gmail.com>
2021-09-19 08:49:13 +0300

    Moved createSchedule function to Appointment class

--- Appointment.cs
+++ Appointment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Drawing.Design;
 using System.Linq;
 using System.Web;
@@ -20,5 +21,50 @@ namespace VR_Web_Project
             this.Date = date;
             this.ParticipantsID = participantsId;
         }
-}
+
+        // Static method to create schedule
+        public static string[][] createSchedule()
+        {
+            DAL DAL = new DAL();
+            string[][] week = new string[8][];
+                week[0] = new string[14];
+
+
+            /* Available times in the system:
+             "08:00", "09:15", "10:30", "11:45", "12:00",
+             "13:15", "14:30", "15:45", "16:00", "17:15",
+             "18:30", "19:45", "20:00", "21:15" */
+            Time time = new Time(8, 0);
+            for (int i = 0; i < 14; i++)
+            {
+                week[0][i] = time.GetTime();
+                time.AddTime(1, 15);
+            }
+
+            for (int i = 1; i < 8; i++)
+            {
+                week[i] = new string[14];
+                for (int j = 0; j < 14; j++)
+                    week[i][j] = "0";
+            }
+
+            string cmdString = "SELECT * FROM Appointment";
+            SqlDataReader reader = DAL.GetReader(cmdString);
+            while (reader.Read())
+            {
+                DateTime date = (DateTime)reader["DateTime"];
+                int days = (date - DateTime.Today).Days + 1 + (int)DateTime.Today.DayOfWeek;
+                if (days > 7) days = days - 7;
+                int hours = (date - DateTime.Today).Hours - 8;
+                if (days > 0 && hours > 0)
+                {
+                    week[days][hours] = ((int)reader["Participants"]).ToString();
+                }
+            }
+
+            reader.Close();
+
+            return week;
+        }
+    }
 }
--- Appointments.aspx.cs
+++ Appointments.aspx.cs
@@ -32,7 +32,7 @@ namespace VR_Web_Project
             DateTime today = DateTime.Today;
             for (int i = 1; i < 8; i++)
             {
-                Button btn =Master.FindControl("TitlePlaceHolder").FindControl("date"+i) as Button;
+                Button btn = Master.FindControl("TitlePlaceHolder").FindControl("date"+i) as Button;
                 btn.Text = today.AddDays(i).ToShortDateString();
             }
         }
@@ -40,47 +40,7 @@ namespace VR_Web_Project
         // Create a schedule using the SQL DB
         private void createSchedule()
         {
-            DAL DAL = new DAL();
-
-            string[][] week = new string[8][];
-            week[0] = new string[14];
-            
-
-            /* Available times in the system:
-             "08:00", "09:15", "10:30", "11:45", "12:00",
-             "13:15", "14:30", "15:45", "16:00", "17:15",
-             "18:30", "19:45", "20:00", "21:15" */
-            Time time = new Time(8, 0);
-            for (int i = 0; i < 14; i++)
-            {
-                week[0][i] = time.GetTime();
-                time.AddTime(1, 15);
-            }
-
-            for (int i = 1; i < 8; i++)
-            {
-                week[i] = new string[14];
-                for (int j = 0; j < 14; j++)
-                    week[i][j] = "0";
-            }
-
-            string cmdString = "SELECT * FROM Appointment";
-            SqlDataReader reader = DAL.GetReader(cmdString);
-            while (reader.Read())
-            {
-                DateTime date = (DateTime)reader["DateTime"];
-                int days = (date - DateTime.Today).Days + 1 + (int)DateTime.Today.DayOfWeek;
-                if (days > 7) days = days - 7;
-                int hours = (date - DateTime.Today).Hours - 8;
-                if (days > 0 && hours > 0)
-                {
-                    week[days][hours] = ((int)reader["Participants"]).ToString();
-                }
-            }
-
-            reader.Close();
-
-            Session["Schedule"] = week;
+            Session["Schedule"] = Appointment.createSchedule();
         }
         private void setOnGrid()
         {
