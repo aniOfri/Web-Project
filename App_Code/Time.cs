450ddcb871f06a20f4f4f8e05779c1349fbeb5e2
aniOfri <ofri0608@gmail.com>
2021-09-21 13:23:47 +0300

    Added comments for the Time and User classes

--- App_Code/Time.cs
+++ App_Code/Time.cs
@@ -1,46 +1,61 @@
 ﻿namespace VR_Web_Project
 {
+    // A SIMPLE TIME STORING-AND-DISPLAYING CLASS
     public class Time
     {
         public int Hour {get;set;}
         public int Minutes { get; set; }
-
         public Time (int hour, int minutes)
         {
             this.Hour = hour;
             this.Minutes = minutes;
         }
 
+        // A public function which adds time to this values
+        // INPUT: int as an hour, int as a minute
+        // OUTPUT: void
         public void AddTime(int hour, int minute)
         {
+            // ADDS THE VALUES TO THIS.HOUR AND THIS.MINUTES
             this.Hour += hour;
             this.Minutes += minute;
 
-            if (this.Minutes == 60)
+            // IF MINUTES EQUALS OR LARGER THAN 60, ADD AN HOUR AND SUBTRACT 60 FROM MINUTES
+            if (this.Minutes >= 60)
             {
-                this.Minutes = 0;
+                this.Minutes -= 60;
                 this.Hour += 1;
             }
 
-            if (this.Hour == 24)
-                this.Hour = 0;
+            // IF HOUR EQUALS OR LARGER THAN 24, SUBTRACT 24 FROM HOURS
+            if (this.Hour >= 24)
+                this.Hour -= 24;
         }
 
+        // A public function which returns a string of time using this' values
+        // INPUT: none
+        // OUTPUT: string as formatted time
         public string GetTime()
         {
+            // DECLARE AN EMPTY RETURN VALUE
             string toReturn = "";
+
+            // IF HOURS LESS THAN 10 PUT A 0 AS A PLACEHOLDER, ELSE, DONT
             if (this.Hour < 10)
                 toReturn += "0" + this.Hour;
             else
                 toReturn += this.Hour.ToString();
             
+            // SEMI COLON TO SPACE SEPERATE MINUTES FROM HOURS
             toReturn += ":";
 
+            // IF MINTUES LESS THAN 10 PUT A 0 AS A PLACEHOLDER, ELSE, DONT
             if (this.Minutes < 10)
                 toReturn += "0" + this.Minutes;
             else
                 toReturn += this.Minutes.ToString();
 
+            // RETURN
             return toReturn;
         }
     }
--- App_Code/User.cs
+++ App_Code/User.cs
@@ -10,7 +10,6 @@ namespace VR_Web_Project
         public string Password { get; set; }
         public string PhoneNumber { get; set; }
         public bool IsManager { get; set; }
-        //private bool IsManager { get; set; }
 
         public User(string username, string password)
         {
@@ -18,81 +17,131 @@ namespace VR_Web_Project
             this.Username = username;
             this.Password = password;
             this.IsManager = false;
-            //this.IsManager = isManager;
         }
 
+        // A private function which returns the next Id
+        // INPUT: none
+        // OUTPUT: int as the Id
         private int CountForId()
         {
             string sql = "SELECT COUNT(*) FROM Member";
-            return DAL.ExecuteScalar(sql);
+            return DAL.ExecuteCounting(sql);
         }
 
+        // A private function which sets Id of this as the Id of the user who logged in using the database
+        // INPUT: none
+        // OUTPUT: void
         private void SetId()
         {
+            // BUILD STRING AS AN SQL COMMAND
             string selectQuery = "SELECT Id FROM Member";
             selectQuery += " WHERE Username ='" + Username + "'";
             selectQuery += " AND Password ='" + Password + "'";
 
+            // GET READER USING DAL
             SqlDataReader reader = DAL.GetReader(selectQuery);
+            
+            // ASSIGN NEW DATA TO Id OF this
             if ((bool)reader.Read())
                 Id = (int)reader["Id"];
 
+            // CLOSE
             reader.Close();
         }
 
-        public bool SetManager()
+        // A private function which sets IsManager of this as the IsManager value of the user who logged in using the database
+        // INPUT: none
+        // OUTPUT: void
+        public void SetManager()
         {
+            // BUILD STRING AS AN SQL COMMAND
             string selectQuery = "SELECT IsManager FROM Member";
             selectQuery += " WHERE Id ='" + Id + "'";
 
+            // GET READER USING DAL
             SqlDataReader reader = DAL.GetReader(selectQuery);
+
+            // ASSIGN NEW DATA TO IsManager OF this
             if ((bool)reader.Read())
                 IsManager = (bool)reader["IsManager"];
 
-            return IsManager;
+            // CLOSE
+            reader.Close();
         }
 
+        // A private function which sets PhoneNumber of this as the PhoneNumber of the user who logged in using the database
+        // INPUT: none
+        // OUTPUT: void
         public void SetPhoneNumber()
         {
+            // BUILD STRING AS AN SQL COMMAND
             string selectQuery = "SELECT PhoneNumber FROM Member";
             selectQuery += " WHERE Id ='" + Id + "'";
 
+            // GET READER USING DAL
             SqlDataReader reader = DAL.GetReader(selectQuery);
+
+            // ASSIGN NEW DATA TO PhoneNumber OF this
             if ((bool)reader.Read())
                 PhoneNumber = (string)reader["PhoneNumber"];
+
+            // CLOSE
+            reader.Close();
         }
 
+        // A public function which attempts to log in the user into the site using this' values
+        // INPUT: none
+        // OUTPUT: bool as success/fail
         public bool Login()
         {
+            // BUILD STRING AS AN SQL COMMAND
             string selectQuery = "SELECT Password FROM Member";
             selectQuery += " WHERE ";
             selectQuery += "Username ='" + Username + "'";
+
+            // CHECKS IF USERNAME EXISTS INSIDE THE DB
             if (DAL.IsExist(selectQuery))
             {
+                // GET DATATABLE OF THE USER WHO ATTEMPTS TO LOG
                 DataTable dt = DAL.ExecuteDataTable(selectQuery);
-                string pass = dt.Rows[0]["password"].ToString().Replace(" ", "");
 
+                // FORMAT AND CHECK THE PASSWORD
+                string pass = dt.Rows[0]["password"].ToString().Replace(" ", "");
                 if (pass == Password)
                 {
+                    // ASSIGN this WITH TRUE DATA
                     SetId();
                     SetManager();
                     SetPhoneNumber();
+
+                    // RETURN SUCCESS
                     return true;
                 }
-                else return false;
             }
+
+            // RETURN FAIL
             return false;
         }
 
+        // A public function which registers the user the to site (inserts data to db)
+        // INPUT: string as a phone number
+        // OUTPUT: bool as success/fail
         public bool Register(string phoneNumber)
         {
+            // BUILD STRING AS AN SQL COMMAND
             string sql = "INSERT INTO Member (Id, Username, PhoneNumber, Password, isManager) VALUES (\'";
             sql += Id + "\', \'" + Username + "\', \'" + phoneNumber + "\', \'" + Password + "\', \'" + false + "\')";
+            
+            // TRY TO EXECUTE COMMAND
             try {
                 DAL.ExecNonQuery(sql);
+                
+                // REGISTERED SUCCESSFULLY, RETURNS TRUE
                 return true;
             }
             catch {
+
+                // THERE WAS AN ERROR, RETURNS FALSE
                 return false;
             }
         }
